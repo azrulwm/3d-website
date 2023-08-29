ce6561d08062b99bfd4ce3d34c5ac13c13f930b9
azrulwm <azrulazmi.wm@gmail.com>
2023-08-29 19:06:14 +0800

    Finalising it before deploying to github

---
+++ frontend/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
+
+# make a copy of index into 404 to fix the issue with reloading on github
+cp index.html 404.html
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:azrulwm/3d-website.git master:gh-pages
+cd -
